bf4353fe91170b9f74556f30f75682999ccf73f9
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-06-29 22:51:25 +0700

    Remove boilerplate from create-react-app

---
+++ src/globalStyles.tsx
@@ -0,0 +1,64 @@
+import { injectGlobal } from "styled-components";
+
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  * {
+    border: 0;
+    box-sizing: inherit;
+    -webkit-font-smoothing: antialiased;
+    font-weight: inherit;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+    text-decoration: none;
+    text-rendering: optimizeLegibility;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+
+  html {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    max-height: 100%;
+    max-width: 100%;
+    box-sizing: border-box;
+    font-size: 16px;
+    line-height: 1.5;
+    background-color: #ffffff;
+    color: #16171a;
+    padding: 0;
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body {
+    display: flex;
+    box-sizing: border-box;
+    flex: auto;
+    align-self: stretch;
+    max-width: 100%;
+    max-height: 100%;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  a {
+    color: currentColor;
+    text-decoration: none;
+  }
+
+  a:hover {
+    cursor: pointer;
+  }
+
+  textarea {
+    resize: none;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+  }
+`;
